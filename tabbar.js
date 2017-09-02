8d0b55d9f0cd5b3ea1697a1cc45701112746b956
Theo Salzmann <dev@on3iro.de>
2017-09-02 11:48:18 +0200

    Script works, but still needs some refactoring

--- tabbar.js
+++ tabbar.js
@@ -1,103 +1,193 @@
-const tabListContainer = document.querySelector('.tabbar-container');
-const tabList = document.querySelector('.list--tabbar');
-let tabListItems = [...tabList.children];
-let tabListContainerWidth = tabListContainer.clientWidth;
-let tabListItemsWidth = getListItemWidth(tabListItems);
-const tabPrevBtn = document.querySelector('.tabbar__prev');
-const tabNextBtn = document.querySelector('.tabbar__next');
-let shiftLeft = 0;
-let shiftRight = 0;
-let currentListItem = 0;
+let state = {}
 
-function getListItemWidth(items) {
-    let sum = 0;
-    items.forEach((item) => {
-        sum = sum + item.clientWidth;
-    });
+/**
+  * TODO
+  */
+const setState = nextState => {
+  const newState = Object.assign({}, state, nextState);
 
-    // 96 is current button width - todo: own variable because it is used on other places
-    return sum + 96;
+  state = newState;
+
+  return newState;
 }
 
-function setActive() {
-    tabListItems.forEach((item) => {
-        item.classList.remove('active');
-    });
-    this.classList.add('active');
+/**
+  * TODO
+  */
+const initialState = {
+  shift: 0,
+  currItemIndex: 0
 }
 
-function checkSlideControls() {
-    if(tabListContainerWidth > tabListItemsWidth) {
-        tabListContainer.classList.remove('show-controls');
-        shiftLeft = 0;
-        shiftRight = 0;
-        currentListItem = 0;
-        tabList.style.transform = "translateX(0)";
-    } else {
-        tabListContainer.classList.add('show-controls');
-    }
+/**
+ * Meant to be used as event listener callback.
+ * Sets active class of currently selected tab.
+ * @param {object} currTab - Tab node which fired the event
+ * @param {array} tabs - Array of tab nodes
+ * @return {undefined}
+ */
+const setActiveTab = (currTab, tabs) => {
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  currTab.classList.add('active');
 }
 
-tabListItems.forEach((item) => {
-    item.addEventListener('click', setActive);
-});
+/**
+  * TODO
+  */
+const checkSlideControls = (tabs, tabList) => {
+  const wrapper = document.querySelector('.tabbar-container');
 
-tabNextBtn.addEventListener('click', () => {
+  const tabListItemsWidth = tabs.reduce((acc, tab) => {
+    return acc + tab.clientWidth;
+  }, 0);
 
-    if(currentListItem < tabListItems.length - 1) {
-        shiftLeft += tabListItems[currentListItem].clientWidth;
-        shiftRight -= tabListItems[currentListItem].clientWidth;
-        tabList.style.transform = "translateX(" + -shiftLeft + "px)";
-        currentListItem++;
+  tabList.style.width = `${tabListItemsWidth}px`;
 
-        if(shiftLeft + tabListItems[currentListItem].clientWidth > tabListContainerWidth - 96) {
-            shiftLeft = 0;
-            shiftRight = 0;
-            currentListItem = 0;
-            tabList.style.transform = "translateX(0)";
-        }
-    } else {
-        shiftLeft = 0;
-        shiftRight = 0;
-        currentListItem = 0;
-        tabList.style.transform = "translateX(0)";
-    }
-});
+  const itemsFillContainer = wrapper.clientWidth < tabListItemsWidth
 
-tabPrevBtn.addEventListener('click', () => {
-    if(currentListItem === 0) {
-        let widthSum = tabListItems[currentListItem].clientWidth;
+  if(itemsFillContainer) {
+    wrapper.classList.add('show-controls');
+  } else {
+    wrapper.classList.remove('show-controls');
+    tabList.style.transform = "translateX(0)";
 
-        while(widthSum < tabListContainerWidth - 96) {
-            widthSum += tabListItems[currentListItem + 1].clientWidth;
-            currentListItem++;
-        }
+    // Reset state
+    setState(initialState)
+  }
+}
 
-        shiftRight -= widthSum - tabListItems[currentListItem].clientWidth;
-        shiftLeft += widthSum - tabListItems[currentListItem].clientWidth;
+/**
+  * TODO
+  */
+const moveRight = (tabs, tabList) => {
+  const { currItemIndex, shift } = state;
+  const wrapper = document.querySelector('.tabbar-container');
 
-        tabList.style.transform = "translateX(" + shiftRight + "px)";
-    }
+  const isLastItem = currItemIndex === tabs.length - 1
+  const shiftIsNegative = shift < 0
+  const newShift = shiftIsNegative
+    ? -(shift - tabs[currItemIndex].clientWidth)
+    : shift + tabs[currItemIndex].clientWidth;
 
-    if(currentListItem > 0) {
-        shiftRight += tabListItems[currentListItem - 1].clientWidth;
-        shiftLeft -= tabListItems[currentListItem - 1].clientWidth;
-        tabList.style.transform = "translateX(" + shiftRight + "px)";
-        currentListItem--;
-    } else {
-        shiftRight = -(tabList.clientWidth - tabListItems[currentListItem].clientWidth);
-        shiftLeft = tabList.clientWidth - tabListItems[currentListItem].clientWidth;
-        currentListItem = tabListItems.length - 1;
-        tabList.style.transform = "translateX(" + shiftRight + "px)";
-    }
-});
+  const newTabListWidth = tabList.clientWidth - shift;
+  const isSmallerThanWrapper = newTabListWidth < wrapper.clientWidth - 96;
 
-console.log(tabListItemsWidth);
-tabList.style.width = tabListItemsWidth + "px";
-checkSlideControls();
+  if (isLastItem || isSmallerThanWrapper) {
+    // Reset state
+    setState(initialState)
+    tabList.style.transform = "translateX(0)";
 
-window.addEventListener('resize', () => {
-    tabListContainerWidth = tabListContainer.clientWidth;
-    //tabListItemsWidth = getListItemWidth(tabListItems);
-    checkSlideControls();
-});
+    return
+  }
+
+  const nextListItem = currItemIndex + 1;
+
+  setState({
+    shift: newShift,
+    currItemIndex: nextListItem
+  })
+
+  tabList.style.transform = `translateX(${-newShift}px)`;
+
+  return
+}
+
+/**
+  * TODO
+  */
+const moveLeft = (tabs, tabList) => {
+  const { shift, currItemIndex } = state;
+  const wrapper = document.querySelector('.tabbar-container');
+
+  const isFirstItem = currItemIndex === 0;
+
+  if (isFirstItem) {
+    // Set index to last item of the list
+    setState({ currItemIndex: tabs.length - 1 });
+
+    const reverseTabs = tabs.slice().reverse();
+
+    const tabListWidth = reverseTabs.reduce((acc, tab, i, list) => {
+      const isLastTab = i === list.length - 1;
+
+      if (isLastTab) return acc.sum;
+
+      if (acc.done) return acc;
+
+      const nextTab = list[i + 1];
+      const sumWithNext = acc.sum + tab.clientWidth + nextTab.clientWidth
+
+      const isLargerThanWrapper = sumWithNext >= wrapper.clientWidth - 96;
+
+      const sum = acc.sum + tab.clientWidth
+
+      if (isLargerThanWrapper) return { sum, done: true };
+
+      setState({ currItemIndex: state.currItemIndex - 1 });
+
+      return { sum, done: false };
+    }, { sum: 0, done: false });
+
+    const newShift = tabList.clientWidth - tabListWidth;
+
+    setState({ shift: newShift })
+
+    tabList.style.transform = `translateX(${-newShift}px)`;
+
+    return
+  }
+
+  const prevListItem = tabs[currItemIndex - 1];
+  const shiftIsPositive = shift > 0;
+
+  const newShift = shiftIsPositive
+    ? -(shift - prevListItem.clientWidth)
+    : shift + prevListItem.clientWidth;
+
+  const prevItemIndex = currItemIndex - 1;
+
+  setState({
+    shift: newShift,
+    currItemIndex: prevItemIndex
+  })
+
+  tabList.style.transform = `translateX(${newShift}px)`;
+}
+
+
+/**
+  * TODO
+  */
+const addButtonEventListeners = (tabs, tabList) => {
+  const tabPrevBtn = document.querySelector('.tabbar__prev');
+  const tabNextBtn = document.querySelector('.tabbar__next');
+
+  tabNextBtn.addEventListener('click', () => moveRight(tabs, tabList));
+  tabPrevBtn.addEventListener('click', () => moveLeft(tabs, tabList));
+}
+
+// IIFE called when script is loaded
+// Immediately invoked function expression
+(initModule = () => {
+  const tabList = document.querySelector('.list--tabbar');
+  const tabListItems = [...tabList.children];
+
+  // Set initial state
+  setState(initialState)
+
+  checkSlideControls(tabListItems, tabList);
+
+  // Add tab event listeners
+  tabListItems.forEach((item) => {
+    item.addEventListener('click', e => setActiveTab(e.target, tabListItems));
+  });
+
+  addButtonEventListeners(tabListItems, tabList)
+
+  window.addEventListener('resize', () => {
+    checkSlideControls(tabListItems,  tabList);
+  });
+})()
